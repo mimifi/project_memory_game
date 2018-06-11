6e21cebb03b166880ee55a939cffd8245ebed531
Mahtab Tadayon <mahtab.tadayon@web.de>
2018-06-11 21:00:27 +0200

    refactor: main responsible just for initialize-delete all other LOC

--- js/main.js
+++ js/main.js
@@ -1,73 +1,3 @@
-//TODO: Rating with stars: rating the player with show the highlighted stars - It's dependent on the  number of movement
-
-//TODO: Timer: Reset and show the time after clicked the reset button 
-
-//TODO: Counter: Show the number of movements - It must be count after each 2 clicks
-
-//TODO: Reset button: Pupup this massage "Let us play a new game and have more fun!"
-
-/**
- * @description Timer is responsible for all timer functionality
- * @constructor
- */
-
-class Timer {
-    constructor() {
-        this.counterSec = 0;
-        this.counterMin = 0;
-        this.timeOut;
-        this.isTimerOn = false;
-    }
-
-    /**
-     * @description represent minute and second and display them
-     */
-    timedCount() {
-        let displayCounterMin;
-        let displayCounterSec;
-        timer.counterSec += 1;
-
-        if (timer.counterSec % 60 === 0) {
-            timer.counterMin += 1
-            timer.counterSec = 0;
-        }
-
-        displayCounterMin = setTextWithTrailingZeroIfNecassery(timer.counterMin);
-        displayCounterSec = setTextWithTrailingZeroIfNecassery(timer.counterSec);
-        /**
-         * @description responsible to show the time in 00:00 format
-         * @function 
-         * @param{number} counter
-         * @returns{string} convert the counter to "00" format for min and sec 
-         */
-
-        function setTextWithTrailingZeroIfNecassery(counter) {
-            return counter < 10 ? "0" + counter : counter;
-        };
-
-        $(".show_time").text(displayCounterMin + ":" + displayCounterSec);
-
-        timer.timeOut = setTimeout(timer.timedCount, 1000);
-    }
-    /**
-     * @description start to count time if boolean true
-     */
-
-    startCount() {
-        if (!this.isTimerOn) {
-            this.isTimerOn = true;
-            this.timedCount();
-        }
-    }
-    /**
-     * @description timer stop to count time
-     */
-
-    stopCount() {
-        clearTimeout(this.timeOut);
-        this.isTimerOn = 0;
-    }
-}
 /**
  * @description to intialize everything and start
  */
@@ -75,33 +5,11 @@ class Timer {
 $(document).ready(() => {
     timer = new Timer();
     stars = new Star();
+    game = new Game();
+
+    game.start();
+
+    $(".flex-container").on("click", ".flex-item", game.checkClickFromFlexItem)
 });
 
-
-/**
- * () => {}
- * function () {}
- */
-
-/**
- *@description initialize the stars and add funtion to these
- *@constructor 
- */
-
-class Star {
-
-    /**
-     * @description: reduce the number of full stars
-     * @method
-     */
-    reduceStar() {
-        $(".fas.fa-star:last").removeClass("fas").addClass("far");
-    }
-    /**
-     * @description: reset the number of star to 5 full stars
-     * @method
-     */
-    resetStars() {
-        $(".far.fa-star").removeClass("far").addClass("fas");
-    }
-}
+//TODO:Restart button reset all stars and the timer
