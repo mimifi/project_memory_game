620fd348aef13e8231fb50046073d7a69bbf2004
Mahtab Tadayon <mahtab.tadayon@web.de>
2018-06-13 20:05:07 +0200

    feat: start the game, start the timer, show and hide random icons

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ class Game {
     /**
      * @description: get a random number between 0 - 16 
      * @constructor
+     * @function
      * @returns: {number} get a random number
      */
     constructor() {
@@ -17,6 +18,30 @@ class Game {
     getRandomIndex() {
         return Math.floor(Math.random() * Math.floor(16));
     }
-    
-    
+
+    /**
+     * @description: show and hide the random icon according to random number, start the timer
+     * @function
+     * @param: {string} element: each element of iconsGrid
+     * @param: {number} index: number of each element in array 
+     */
+    start() {
+        this.clickCounter = 0;
+        this.indexArray = [];
+        const ctx = this;
+        timer.startCount();
+
+        this.iconsGrid.forEach(function (element, index) {
+            const icon = $('<i class="fas ' + element + '"></i>').hide();
+            let randomIndex = ctx.getRandomIndex();
+
+            while (ctx.indexArray.includes(randomIndex)) {
+                randomIndex = ctx.getRandomIndex();
+            }
+
+            $($(".flex-item")[randomIndex]).append(icon);
+            ctx.indexArray.push(randomIndex);
+        });
+    }
+
 };
