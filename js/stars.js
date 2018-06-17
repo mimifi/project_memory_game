f029461441b71d888db3ab199069a433e7345d70
Mahtab Tadayon <mahtab.tadayon@web.de>
2018-06-17 18:44:22 +0200

    fix: just reduce number of stars if it's more than 0.

--- js/stars.js
+++ js/stars.js
@@ -14,8 +14,11 @@ class Star {
      */
     reduceStar() {
 
-        $(".fas.fa-star:last").removeClass("fas").addClass("far");
-        this.numStars--;
+        if (this.numStars > 0) {
+            $(".fas.fa-star:last").removeClass("fas").addClass("far");
+            this.numStars--;
+        }
+
     }
     /**
      * @description: reset the number of star to 5 full stars
